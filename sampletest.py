d53f68c1fdf1f5304a07e127feb2d31c3d483e55
jahanRazh <jahanrazh@example.com>
2023-12-03 14:07:19 +0530

    first commit

--- sampletest.py
+++ sampletest.py
@@ -4,22 +4,22 @@
 def add(x, y):
     return x + y
 
-def subtract(x, y):
+def sub(x, y):
     return x - y
 
-def multiply(x, y):
+def mul(x, y):
     return x * y
 
-def divide(x, y):
+def div(x, y):
     if y == 0:
-        print("Error: Cannot divide by zero.")
+        print("float division by zero")
         return None
     return x / y
 
 def power(x, y):
     return x ** y
 
-def remainder(x, y):
+def remind(x, y):
     if y == 0:
         print("Error: Cannot calculate remainder with zero.")
         return None
@@ -30,27 +30,58 @@ def remainder(x, y):
 #This function should cover Task 1 (Section 2) and Task 3
 
 def select_op(choice):
-    if choice == '+':
-        return add
-    elif choice == '-':
-        return subtract
-    elif choice == '*':
-        return multiply
-    elif choice == '/':
-        return divide
-    elif choice == '^':
-        return power
-    elif choice == '%':
-        return remainder
-    elif choice == '#':
+    if (choice == '#'):
         return -1  # Special code to indicate program termination
     elif choice == '$':
         print("Resetting...")
-        return None  # Special code to indicate program reset
+        return 0  # Special code to indicate program reset
+    elif (choice in('+','-','*','/','^','%')):
+        while True:
+            num1 = str(input("Enter first number: "))
+            print(num1)
+            if num1.endswith('$'):
+                return 0
+            if num1.endswith('#'):
+                return -1
+            try:
+                x1 = float(num1)
+                break
+            except:
+                print("Not a valid number,please enter again")
+                continue
+        
+        while True:
+            num2 = str(input("Enter second number: "))
+            print(num2)
+            if num2.endswith('$'):
+                return 0
+            if num2.endswith('#'):
+                return -1
+            try:
+                x2 = float(num2)
+                break
+            except:
+                print("Not a valid number,please enter again")
+                continue
+    
+        if (choice == '+'):
+            print(x1, "+", x2, "=", add(x1,x2))
+        elif (choice == '-'):
+            print(x1, "-", x2, "=", sub(x1,x2))
+        elif (choice == '*'):
+            print(x1, "*", x2, "=", mul(x1,x2))
+        elif (choice == '/'):
+            print(x1, "/", x2, "=", div(x1,x2))
+        elif (choice == '^'):
+            print(x1, "^", x2, "=", power(x1,x2))
+        elif (choice == '%'):
+            print(x1, "%", x2, "=", remind(x1,x2))
+        else:
+            print("Invalid input. Please enter a valid operation.")
+            return None
     else:
-        print("Invalid input. Please enter a valid operation.")
-        return None
-
+        print("Unrecognized operation")
+        
 #End the select_op(choice) function here
 #-------------------------------------
 
@@ -70,19 +101,8 @@ while True:
     # take input from the user
     choice = input("Enter choice(+,-,*,/,^,%,#,$): ")
     print(choice)
-    operation = select_op(choice)
-    if operation == -1:
+    if (select_op(choice) == -1):
         # program ends here
         print("Done. Terminating")
         exit()
-    elif operation is not None:
-        # valid operation, proceed with calculation
-        if operation == add or operation == subtract or operation == multiply or operation == divide or operation == power or operation == remainder:
-            x = float(input("Enter first operand: "))
-            y = float(input("Enter second operand: "))
-            result = operation(x, y)
-            if result is not None:
-                print(f"Result: {result}")
-    elif operation is None:
-        # Reset the program
-        continue
+  
